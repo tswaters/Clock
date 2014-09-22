e59e4e314212db9e13b03724e14666534b9bb9a5
Tyler Waters <tyler.waters@gmail.com>
2014-09-21 19:17:01 -0700

    Removing test converter.

--- Clock/js/classes/converters.js
+++ Clock/js/classes/converters.js
@@ -5,11 +5,6 @@
 
         'visibility': WinJS.Binding.converter(function (val) {
             return val ? "block" : "none";
-        }),
-
-        'test': WinJS.Binding.converter(function (val) {
-            console.log('Binding converter called with' + val);
-            return val;
         })
 
     });
